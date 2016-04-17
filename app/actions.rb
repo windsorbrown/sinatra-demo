ec50253d1716e64f73b75d31e901e22763cc42e7
windsorbrown <rahmanzr@gmail.com>
2016-04-16 22:00:09 -0700

    finale

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,37 @@
 get '/' do
   erb :index
 end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+get '/messages/new' do
+  #@messages = Message.all
+  erb :'messages/new'
+end
+
+post '/messages' do
+
+    @message = Message.new(
+      content: params[:content],
+      author: params[:author],
+      url: params[:url]
+      )
+    if @message.save
+      redirect '/messages'
+    else
+    erb :'messages/new'
+  end
+end
+
+  get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+  get '/messages/author/:author' do
+    @message_author = Message.where(author: params[:author])
+    erb :'messages/author'
+  end
